e6f67a73c6b53ca48a6ca6153bc4adff07d99bf5
Sergio A Valdes Rabelo <sergiovades2409@gmail.com>
2020-09-27 18:05:08 -0600

    Create Formulario component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Header from './components/header'
+import Header from './components/header';
+import styled from '@emotion/styled';
+import Formulario from './components/formulario';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding:3rem;
+`;
+
 
 function App() {
   return (
-    <Header 
-      titulo="Cotizador de Seguros"
-    />    
+    <Contenedor>
+      <Header 
+        titulo="Cotizador de Seguros"
+      />
+
+      <ContenedorFormulario>
+          <Formulario />
+      </ContenedorFormulario>
+
+    </Contenedor>    
   );
 }
 
